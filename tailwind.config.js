b3e2d217bebae05ce1a42284cfe5af215ef1c75b
luismeza8 <angel841bv@gmail.com>
2024-07-22 18:26:42 -0700

    background color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/*.html",".*.html"],
+  content: ["./src/**/*.{html,js}", "./src/*.html","*.html"],
   theme: {
     extend: {},
   },
